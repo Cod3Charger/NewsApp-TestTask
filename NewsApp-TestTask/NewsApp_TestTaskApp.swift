8a831d3319c21d8d6c7127fde7c74f4513adbd38
sergeidashko <dashkosergeiit@gmail.com>
2024-09-18 09:25:33 +0300

    NewsApp-TestTask create project

---
+++ NewsApp-TestTask/NewsApp_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_TestTaskApp.swift
+//  NewsApp-TestTask
+//
+//  Created by Сергей Дашко on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
